6ee9edbe07a5fb49e334d331c43824e5c479ef2f
Cheeras <31730256+Cheeras@users.noreply.github.com>
2023-01-10 10:52:27 +0530

    added notes about arrays (#1)
    Co-authored-by: Sankar Cheerala <shankarchirala@gmail.com>

---
+++ ArraysDSA/src/com/arrays/RemoveDuplicates.java
@@ -0,0 +1,16 @@
+package com.arrays;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+
+		int[] arr = {22,18,20,22,1,34,20};
+		//ouput {22,18,20,1,34,20};
+		removeDuplicates(arr);
+	}
+	
+	static void removeDuplicates(int[] arr) {
+		
+	}
+
+}
